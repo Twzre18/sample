334242bd50c251fa26617f65179d63d81ea24af2
Twizere <rukisaie@outlook.com>
2018-03-10 10:21:54 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   #get 'stocks/index'
-  get '/stocks', to: 'stocks#index' # add this line
+  #get '/stocks', to: 'stocks#index' # add this line
 
   #get 'blogs/index'
-  get '/', to: 'blogs#index' # add this line
+  get '/blogs', to: 'blogs#index' # add this line
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :blogs do
